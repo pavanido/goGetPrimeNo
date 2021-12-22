c474ddf0ef72c30e1cfcb7b08b5a7c1207de6d57
Pradeep Malineni <pradeep.malineni@hotmail.com>
2021-12-22 22:58:44 +0530

    Test

---
+++ v4/calculate/prime.go
@@ -0,0 +1,9 @@
+package calculate
+
+import "fmt"
+
+func GetPrimeNumber(number int, number2 int) int {
+	fmt.Println("Received Request from client to calculate package under GetPrimeNumber module", number)
+	return number + number2
+
+}
---
+++ v4/go.mod
@@ -0,0 +1,3 @@
+module github.com/pavanido/goGetPrimeNo/v4
+
+go 1.16
